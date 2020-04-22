de54c4679eef14660bb5c271d9cefa6615ce891f
SebastianHafner <hafnersailing@gmail.com>
2020-04-22 16:04:07 +0200

    refactored

--- make_xys.py
+++ make_xys.py
@@ -4,109 +4,115 @@ from skimage.transform import rotate, resize
 import os
 import cv2
 import pandas as pd
+from pathlib import Path
 
-train_areas = ['abudhabi', 'beihai', 'aguasclaras', 'beirut', 'bercy', 'bordeaux', 'cupertino',
-
-                 'hongkong', 'mumbai', 'nantes', 'rennes', 'saclay_e', 'pisa', 'rennes']
-
-FOLDER='../Labels/'
-
-step=6
-patch_s=32
 
 def shuffle(vector):
-  vector = np.asarray(vector)
-  p=np.random.permutation(len(vector))
-  vector=vector[p]
-  return vector
+    vector = np.asarray(vector)
+    p = np.random.permutation(len(vector))
+    vector = vector[p]
+    return vector
 
 
 def sliding_window_train(i_city, labeled_areas, label, window_size, step):
-    city=[]
-    fpatches_labels=[]
+    city = []
+    fpatches_labels = []
 
-    x=0
-    while (x!=label.shape[0]):
-     y=0
-     while(y!=label.shape[1]):
+    x = 0
+    while x != label.shape[0]:
+        y = 0
+        while y != label.shape[1]:
 
-               if (not y+window_size > label.shape[1]) and (not x+window_size > label.shape[0]):
-                line=np.array([x,y, labeled_areas.index(i_city), 0]) # (x,y) are the saved coordinates, 
-                                                                     # labeled_areas.index(i_city)... are the image ids, e.g according to train_areas,
-                                                                           #the indice for abudhabi in the list is 0, for beihai it is 1, for beirut is 3, etc..
-                                                                     # the fourth element which has been set as 0, represents the transformadion index,
-                                                                           #which in this case indicates that no data augmentation will be performed for the
-                                                                           #specific patch 
+            if (not y + window_size > label.shape[1]) and (not x + window_size > label.shape[0]):
+                line = np.array([x, y, labeled_areas.index(i_city), 0])
+                # (x,y) are the saved coordinates,
+                # labeled_areas.index(i_city)... are the image ids, e.g according to train_areas,
+                # the indice for abudhabi in the list is 0, for beihai it is 1, for beirut is 3, etc..
+                # the fourth element which has been set as 0, represents the transformadion index,
+                # which in this case indicates that no data augmentation will be performed for the
+                # specific patch
+
                 city.append(line)
 
-                ##############CONDITIONS####################################
+                # CONDITIONS
                 new_patch_label = label[x:x + window_size, y:y + window_size]
-                ff=np.where(new_patch_label==2)
-                perc = ff[0].shape[0]/float(window_size*window_size)
-                if ff[0].shape[0]==0:
-                       stride=window_size
+                ff = np.where(new_patch_label == 2)
+                perc = ff[0].shape[0] / float(window_size * window_size)
+                if ff[0].shape[0] == 0:
+                    stride = window_size
                 else:
-                       stride=step
-                if perc>=0.05: #if percentage of change exceeds a threshold, perform data augmentation on this patch
-                               #Below, 1, 2, 3 are transformation indexes that will be used by the custom dataloader
-                               #to perform various rotations
-                       line=np.array([x,y, labeled_areas.index(i_city), 1])
-                       city.append(line)
-                       line=np.array([x,y, labeled_areas.index(i_city), 2])
-                       city.append(line)
-                       line=np.array([x,y, labeled_areas.index(i_city), 3])
-                       city.append(line)
-                 ###############CONDITIONS####################################
+                    stride = step
+                if perc >= 0.05:
+                    # if percentage of change exceeds a threshold, perform data augmentation on this patch
+                    # Below, 1, 2, 3 are transformation indexes that will be used by the custom dataloader
+                    # to perform various rotations
+                    line = np.array([x, y, labeled_areas.index(i_city), 1])
+                    city.append(line)
+                    line = np.array([x, y, labeled_areas.index(i_city), 2])
+                    city.append(line)
+                    line=np.array([x, y, labeled_areas.index(i_city), 3])
+                    city.append(line)
+                 # CONDITIONS
 
-               if y + window_size == label.shape[1]:
-                  break
+            if y + window_size == label.shape[1]:
+                break
 
-               if y + window_size > label.shape[1]:
+            if y + window_size > label.shape[1]:
                 y = label.shape[1] - window_size
-               else:
-                y = y+stride
+            else:
+                y = y + stride
 
-     if x + window_size == label.shape[0]:
-        break
+        if x + window_size == label.shape[0]:
+            break
 
-     if x + window_size > label.shape[0]:
-       x = label.shape[0] - window_size
-     else:
-      x = x+stride
+        if x + window_size > label.shape[0]:
+            x = label.shape[0] - window_size
+        else:
+            x = x + stride
 
     return np.asarray(city)
 
 
-cities=[]
-for i_city in train_areas:
- path=FOLDER+'{}/cm/{}-cm.tif'.format(i_city, i_city)
- im_name = os.path.basename(path)
- print('icity', i_city)
- train_gt = io.imread(path)
- xy_city =  sliding_window_train(i_city, train_areas, train_gt, patch_s, step)
- cities.append(xy_city)
+if __name__ == '__main__':
 
-#from all training (x,y) locations, divide 4/5 for training and 1/5 for validation
-final_cities = np.concatenate(cities, axis=0)
-size_len = len(final_cities)
-portion=size_len/5
-final_cities=shuffle(final_cities)
-final_cities_train = final_cities[:4*portion]
-final_cities_val = final_cities[4*portion:]
+    train_areas = ['abudhabi', 'beihai', 'aguasclaras', 'beirut', 'bercy', 'bordeaux', 'cupertino',
+                   'hongkong', 'mumbai', 'nantes', 'rennes', 'saclay_e', 'pisa', 'rennes']
 
+    FOLDER = Path('C:/Users/hafne/urban_change_detection/data/Onera/labels/')
 
-##save train to csv file
-df = pd.DataFrame({'X': list(final_cities_train[:,0]),
-                   'Y': list(final_cities_train[:,1]),
-                   'image_ID': list(final_cities_train[:,2]),
-                   'transform_ID': list(final_cities_train[:,3]),
-                   })
-df.to_csv('../myxys_train.csv', index=False, columns=["X", "Y", "image_ID", "transform_ID"])
+    step = 6
+    patch_s = 32
 
+    cities = []
+    for i_city in train_areas:
+        file = FOLDER / i_city / 'cm' / f'{i_city}-cm.tif'
+        print('icity', i_city)
+        train_gt = io.imread(file)
+        xy_city = sliding_window_train(i_city, train_areas, train_gt, patch_s, step)
+        cities.append(xy_city)
 
-df = pd.DataFrame({'X': list(final_cities_val[:,0]),
-                   'Y': list(final_cities_val[:,1]),
-                   'image_ID': list(final_cities_val[:,2]),
-                   'transform_ID': list(final_cities_val[:,3]),
-                   })
-df.to_csv('../myxys_val.csv', index=False, columns=["X", "Y", "image_ID", "transform_ID"])
+    # from all training (x,y) locations, divide 4/5 for training and 1/5 for validation
+    final_cities = np.concatenate(cities, axis=0)
+    size_len = len(final_cities)
+    portion = int(size_len / 5)
+    final_cities = shuffle(final_cities)
+    final_cities_train = final_cities[:-portion]
+    final_cities_val = final_cities[-portion:]
+
+    # save train to csv file
+    df = pd.DataFrame({'X': list(final_cities_train[:, 0]),
+                       'Y': list(final_cities_train[:, 1]),
+                       'image_ID': list(final_cities_train[:, 2]),
+                       'transform_ID': list(final_cities_train[:, 3]),
+                       })
+    train_file = FOLDER.parent / 'myxys_train.csv'
+    df.to_csv(str(train_file), index=False, columns=["X", "Y", "image_ID", "transform_ID"])
+
+    # save val to csv file
+    df = pd.DataFrame({'X': list(final_cities_val[:, 0]),
+                       'Y': list(final_cities_val[:, 1]),
+                       'image_ID': list(final_cities_val[:, 2]),
+                       'transform_ID': list(final_cities_val[:, 3]),
+                       })
+    val_file = FOLDER.parent / 'myxys_val.csv'
+    df.to_csv(str(val_file), index=False, columns=["X", "Y", "image_ID", "transform_ID"])
--- preprocess.py
+++ preprocess.py
@@ -1,20 +1,20 @@
 import cv2
 from skimage import io
 import numpy as np
-import os
-import glob
+from pathlib import Path
+
 
 def stretch_8bit(band, lower_percent=2, higher_percent=98):
- a = 0
- b = 255
- real_values = band.flatten()
- real_values = real_values[real_values > 0]
- c = np.percentile(real_values, lower_percent)
- d = np.percentile(real_values, higher_percent)
- t = a + (band - c) * (b - a) / float(d - c)
- t[t<a] = a
- t[t>b] = b
- return t.astype(np.uint8) / 255.
+    a = 0
+    b = 255
+    real_values = band.flatten()
+    real_values = real_values[real_values > 0]
+    c = np.percentile(real_values, lower_percent)
+    d = np.percentile(real_values, higher_percent)
+    t = a + (band - c) * (b - a) / float(d - c)
+    t[t < a] = a
+    t[t > b] = b
+    return t.astype(np.uint8) / 255.
 
 
 def histogram_match(source, reference, match_proportion=1.0):
@@ -57,63 +57,74 @@ def histogram_match(source, reference, match_proportion=1.0):
 
     return target.reshape(orig_shape)
 
-IMG_FOLDER = '../images/' #folder of the form ./IMGS_PREPROCESSED/abudhabi/imgs_1/..(13 tif 2D images of sentinel channels)..
-                                 #           ./IMGS_PREPROCESSED/abudhabi/imgs_2/..(13 tif 2D images of sentinel channels)..
-                                 #           ....
-                                 #           ./IMGS_PREPROCESSED/abudhabi/imgs_n/..(13 tif 2D images of sentinel channels)..
-                                 #           where n = number of dates
 
-nb_dates = [1,2,3,4,5] ##here you specify which dates you want to use
-channels = ['B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B8A', 'B09', 'B10', 'B11', 'B12']
+if __name__ == '__main__':
+    IMG_FOLDER = Path('C:/Users/hafne/urban_change_detection/data/Onera/images/')
+    # folder of the form ./IMGS_PREPROCESSED/abudhabi/imgs_1/..(13 tif 2D images of sentinel channels)..
+    # ./IMGS_PREPROCESSED/abudhabi/imgs_2/..(13 tif 2D images of sentinel channels)..
+    # ....
+    # ./IMGS_PREPROCESSED/abudhabi/imgs_n/..(13 tif 2D images of sentinel channels)..
+    # where n = number of dates
 
-#all areas of the OSCD dataset
-all_areas = ['abudhabi', 'aguasclaras', 'beihai', 'beirut', 'bercy', 'bordeaux', 'brasilia', 'chongqing',
-        'cupertino', 'dubai', 'hongkong', 'lasvegas', 'milano', 'montpellier', 'mumbai', 'nantes',
-        'norcia', 'paris', 'pisa', 'rennes', 'rio', 'saclay_e', 'saclay_w', 'valencia']
+    # here you specify which dates you want to use
+    nb_dates = [1, 2]
+    channels = ['B01', 'B02', 'B03', 'B04', 'B05', 'B06', 'B07', 'B08', 'B8A', 'B09', 'B10', 'B11', 'B12']
 
-DESTINATION_FOLDER = 'IMGS_PREPROCESSED'
-os.mkdir('./'+DESTINATION_FOLDER)
+    # all areas of the OSCD dataset
+    all_areas = ['abudhabi', 'aguasclaras', 'beihai', 'beirut', 'bercy', 'bordeaux', 'brasilia', 'chongqing',
+                 'cupertino', 'dubai', 'hongkong', 'lasvegas', 'milano', 'montpellier', 'mumbai', 'nantes',
+                 'norcia', 'paris', 'pisa', 'rennes', 'rio', 'saclay_e', 'saclay_w', 'valencia']
 
-for i_path in all_areas:
- print(i_path)
+    DESTINATION_FOLDER = Path('C:/Users/hafne/urban_change_detection/data/Onera/images_preprocessed/')
+    if not DESTINATION_FOLDER.exists():
+        DESTINATION_FOLDER.mkdir()
 
- date_folders = []
- for nd in nb_dates:
-     date_folders.append(list(glob.glob(os.path.join(IMG_FOLDER +i_path+ '/imgs_{}/*.tif'.format(str(nd))))))
+    for i_path in all_areas:
+        print(i_path)
 
- #B02 channel has the same dimensions with the groundtruth for the labeled images.
- #So we keep it to reshape the rest of the channels for both labeled images and nonlabeled images
- gts = [s for s in date_folders[0] if 'B02' in s]
- gts = io.imread(gts[0])
+        date_folders = []
+        for nd in nb_dates:
+            temp_path = IMG_FOLDER / i_path / f'imgs_{nd}'
+            files = [file for file in temp_path.glob('**/*')]
+            date_folders.append(files)
 
- os.mkdir('./'+DESTINATION_FOLDER+'/'+i_path+'/')
+        # B02 channel has the same dimensions with the groundtruth for the labeled images.
+        # So we keep it to reshape the rest of the channels for both labeled images and nonlabeled images
+        gts = [s for s in date_folders[0] if 'B02' in str(s)]
+        gts = io.imread(gts[0])
 
- for nd in nb_dates:
-      print('date', nd)
-      imgs = []
-      if nd ==1:
-         for ch in channels:
-             im = [s for s in date_folders[nd-1] if ch in s]
-             im=io.imread(im[0])
-             im[im>5500]=5500
-             im=stretch_8bit(im)
-             im=cv2.resize(im, (gts.shape[1], gts.shape[0]))
-             im=np.reshape(im, (gts.shape[0], gts.shape[1], 1))
-             imgs.append(im)
-         imgs0 = imgs
-      else:
+        temp_path = DESTINATION_FOLDER / i_path
+        if not temp_path.exists():
+            temp_path.mkdir()
 
-         for ch in channels:
-             im = [s for s in date_folders[nd-1] if ch in s]
-             im=io.imread(im[0])
-             im[im>5500]=5500
-             im=stretch_8bit(im)
-             im=histogram_match(im, imgs0[channels.index(ch)])
-             im=cv2.resize(im, (gts.shape[1], gts.shape[0]))
-             im=np.reshape(im, (gts.shape[0], gts.shape[1], 1))
-             imgs.append(im)
+        for nd in nb_dates:
+            print('date', nd)
+            imgs = []
+            if nd == 1:
+                for ch in channels:
+                    im = [s for s in date_folders[nd-1] if ch in str(s)]
+                    im = io.imread(im[0])
+                    im[im > 5500] = 5500
+                    im = stretch_8bit(im)
+                    im = cv2.resize(im, (gts.shape[1], gts.shape[0]))
+                    im = np.reshape(im, (gts.shape[0], gts.shape[1], 1))
+                    imgs.append(im)
+                imgs0 = imgs
+            else:
 
-      im_merge = np.stack(imgs, axis=2)
-      im_merge = np.asarray(im_merge)
-      im_merge = np.reshape(im_merge, (im_merge.shape[0], im_merge.shape[1], im_merge.shape[2]))
-      np.save('./'+ DESTINATION_FOLDER+'/'+i_path+'/'+i_path+'_{}.npy'.format(str(nd)), im_merge)
+                for ch in channels:
+                    im = [s for s in date_folders[nd-1] if ch in str(s)]
+                    im = io.imread(im[0])
+                    im[im > 5500] = 5500
+                    im = stretch_8bit(im)
+                    im = histogram_match(im, imgs0[channels.index(ch)])
+                    im = cv2.resize(im, (gts.shape[1], gts.shape[0]))
+                    im = np.reshape(im, (gts.shape[0], gts.shape[1], 1))
+                    imgs.append(im)
+
+            im_merge = np.stack(imgs, axis=2)
+            im_merge = np.asarray(im_merge)
+            im_merge = np.reshape(im_merge, (im_merge.shape[0], im_merge.shape[1], im_merge.shape[2]))
+
+            im_file = DESTINATION_FOLDER / i_path / f'{i_path}_{nd}.npy'
+            np.save(str(im_file), im_merge)
