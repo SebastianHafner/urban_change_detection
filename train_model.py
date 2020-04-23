28d3b1175cacbedd991af6f693cb7b543fa4720f
SebastianHafner <hafnersailing@gmail.com>
2020-04-23 20:13:19 +0200

    initial commit for new training

---
+++ train_model.py
@@ -0,0 +1,13 @@
+import json
+
+from download_manager import args
+from download_manager.config import config
+
+
+
+def setup(args):
+    cfg = config.new_config()
+    cfg.merge_from_file(f'configs/{args.config_file}.yaml')
+    cfg.merge_from_list(args.opts)
+    cfg.NAME = args.config_file
+    return cfg
