4bd603ca4f1105f0bd2f9266fe60a44a988c0f6c
SebastianHafner <hafnersailing@gmail.com>
2020-05-22 19:07:29 +0200

    evaluation with test time augmentation

--- evaluation.py
+++ evaluation.py
@@ -43,7 +43,7 @@ def load_net(cfg, net_file):
     return net
 
 
-def evaluate_patches(root_dir: Path, cfg_file: Path, net_file: Path, dataset: str = 'test', n: int = 10,
+def visual_evaluation(root_dir: Path, cfg_file: Path, net_file: Path, dataset: str = 'test', n: int = 10,
                      save_dir: Path = None, label_pred_only: bool = False):
 
     mode = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -115,7 +115,7 @@ def evaluate_patches(root_dir: Path, cfg_file: Path, net_file: Path, dataset: st
             plt.close()
 
 
-def tta(cfg_file: Path, net_file: Path):
+def numeric_evaluation(cfg_file: Path, net_file: Path):
 
     mode = 'cuda' if torch.cuda.is_available() else 'cpu'
     device = torch.device(mode)
@@ -133,59 +133,64 @@ def tta(cfg_file: Path, net_file: Path):
     }
     dataloader = torch_data.DataLoader(dataset, **dataloader_kwargs)
 
+    pred_results = {'city': [], 'label': [], 'pred': [], 'tta_pred': []}
     with torch.no_grad():
         net.eval()
-        all_data = []
         for step, batch in enumerate(dataloader):
-            y_preds = []
 
             t1_img = batch['t1_img'].to(device)
             t2_img = batch['t2_img'].to(device)
             y_true = batch['label'].to(device)
-            y_true = y_true[0,].to(device)
+
+            pred_results['label'].append(y_true.flatten())
+            pred_results['city'].append(batch['city'][0])
+
+            # container for test time augmentation (tta)
+            y_preds_tta = []
 
             # rotations
             for k in range(4):
                 t1_img_rot = torch.rot90(t1_img, k, (2, 3))
                 t2_img_rot = torch.rot90(t2_img, k, (2, 3))
                 y_pred = net(t1_img_rot, t2_img_rot)
-                y_pred = torch.sigmoid(y_pred)
-                if k == 0:
-                    y_pred_test = y_pred > cfg.THRESH
-                    y_pred_test = y_pred_test[0, ]
-                    f1 = eval.f1_score(y_true, y_pred_test)
-                    print(batch['city'])
-                    print(f1.item())
+                y_pred = torch.rot90(y_pred, 4 - k, (2, 3))
+                y_pred = torch.sigmoid(y_pred) > cfg.THRESH
+                y_pred = y_pred.float()
 
-                y_pred = torch.rot90(y_pred, 4-k, (2, 3))
-                y_preds.append(y_pred)
+                # normal predictiion (without test time augmentation)
+                if k == 0:
+                    pred_results['pred'].append(y_pred.flatten())
+
+                y_preds_tta.append(y_pred)
 
             # flips
             for flip in [(2, 3), (3, 2)]:
                 t1_img_flip = torch.flip(t1_img, flip)
                 t2_img_flip = torch.flip(t1_img, flip)
                 y_pred = net(t1_img_flip, t2_img_flip)
-                y_pred = torch.sigmoid(y_pred)
                 y_pred = torch.flip(y_pred, flip)
-                y_preds.append(y_pred)
+                y_pred = torch.sigmoid(y_pred) > cfg.THRESH
+                y_preds_tta.append(y_pred.float())
 
-            def torch2numpy(y_pred):
-                y_pred = y_pred.cpu().detach().numpy()[0,]
-                y_pred = y_pred > cfg.THRESH
-                y_pred = y_pred.transpose((1, 2, 0)).astype('uint8')
-                return y_pred[:, :, 0]
+            y_preds_tta = torch.cat(y_preds_tta, dim=1)
+            y_pred_tta = torch.mean(y_preds_tta, dim=1, keepdim=True)
+            pred_results['tta_pred'].append(y_pred_tta.flatten())
 
-            y_preds = [torch2numpy(y_pred) for y_pred in y_preds]
-            y_pred = np.stack(y_preds)
-            y_pred = np.mean(y_pred, axis=0)
-            y_pred = y_pred > 0
-            y_pred = TF.to_tensor(y_pred).to(device)
-            f1 = eval.f1_score(y_true, y_pred)
-            print(batch['city'])
-            print(f1.item())
+        print('summary')
 
+        labels = torch.cat(pred_results['label'], dim=0)
+        predictions = torch.cat(pred_results['pred'], dim=0)
+        predictions_tta = torch.cat(pred_results['tta_pred'], dim=0)
 
+        f1 = eval.f1_score(labels, predictions, dim=0)
+        print(f'{f1.item():.3f}')
 
+        thresholds = np.linspace(0, 1, 11)
+        for ts in thresholds:
+            predictions_tta_ts = predictions_tta > ts
+            predictions_tta_ts = predictions_tta_ts.float()
+            f1_tta = eval.f1_score(labels, predictions_tta_ts, dim=0)
+            print(f'{f1_tta.item():.3f} ({ts:.1f})')
 
 
 if __name__ == '__main__':
@@ -195,11 +200,11 @@ if __name__ == '__main__':
     STORAGE_DIR = Path('/storage/shafner/urban_change_detection')
 
     dataset = 'OSCD_dataset'
-    cfg = 'baseline'
+    cfg = 'baseline_lowlr'
 
     cfg_file = CFG_DIR / f'{cfg}.yaml'
     net_file = NET_DIR / cfg / 'best_net.pkl'
 
-    # evaluate_patches(STORAGE_DIR, cfg_file, net_file, 'test', 100, label_pred_only=True)
-    tta(cfg_file, net_file)
+    # visual_evaluation(STORAGE_DIR, cfg_file, net_file, 'test', 100, label_pred_only=True)
+    numeric_evaluation(cfg_file, net_file)
 
